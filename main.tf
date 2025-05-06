9ec11da8b88babba6f83d697ac633565e554d8ad
Adedeji Mustapha <adedeji.mustapha.2018@gmail.com>
2025-05-06 20:03:29 +0100

    added setup for db server

--- main.tf
+++ main.tf
@@ -3,9 +3,11 @@ resource "aws_key_pair" "deployer" {
   public_key = file(var.public_key_path)
 }
 
-resource "aws_security_group" "prestashop_sg" {
-  name        = "prestashop-sg"
-  description = "Allow HTTP, HTTPS, and SSH"
+# Security Group for the Web Server
+resource "aws_security_group" "web_sg" {
+  name        = "prestashop-web-sg"
+  description = "Allow SSH and HTTP"
+  vpc_id      = var.vpc_id
 
   ingress {
     description = "SSH"
@@ -23,10 +25,32 @@ resource "aws_security_group" "prestashop_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Security Group for the DB Server
+resource "aws_security_group" "db_sg" {
+  name        = "prestashop-db-sg"
+  description = "Allow MySQL from Web Server"
+  vpc_id      = var.vpc_id
+
   ingress {
-    description = "HTTPS"
-    from_port   = 443
-    to_port     = 443
+    description = "MySQL"
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    security_groups = [aws_security_group.web_sg.id]
+  }
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -39,15 +63,29 @@ resource "aws_security_group" "prestashop_sg" {
   }
 }
 
-resource "aws_instance" "prestashop" {
-  ami                         = "ami-041d287dbeca860a8"  # Ubuntu 20.04 LTS (Free-tier eligible)
-  instance_type               = var.instance_type
-  key_name                    = aws_key_pair.deployer.key_name
-  vpc_security_group_ids      = [aws_security_group.prestashop_sg.id]
-  subnet_id                   = var.subnet_id
+# Web Server
+resource "aws_instance" "web_server" {
+  ami           = "ami-041d287dbeca860a8" # Ubuntu 22.04 LTS in af-south-1
+  instance_type = var.instance_type
+  key_name      = var.key_name
+  subnet_id     = var.subnet_id
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
   associate_public_ip_address = true
 
   tags = {
-    Name = "Prestashop-Server"
+    Name = "PrestaShop-Web"
+  }
+}
+
+# Database Server
+resource "aws_instance" "db_server" {
+  ami           = "ami-041d287dbeca860a8"
+  instance_type = var.instance_type
+  key_name      = var.key_name
+  subnet_id     = var.subnet_id
+  vpc_security_group_ids = [aws_security_group.db_sg.id]
+
+  tags = {
+    Name = "PrestaShop-DB"
   }
 }
--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,19 @@
 output "instance_public_ip" {
   description = "Public IP of the PrestaShop server"
-  value       = aws_instance.prestashop.public_ip
+  value       = aws_instance.web_server.public_ip
 }
 
 output "instance_id" {
   description = "ID of the EC2 instance"
-  value       = aws_instance.prestashop.id
+  value       = aws_instance.web_server.id
+}
+
+output "db_instance_private_ip" {
+  description = "Private IP of the database server instance"
+  value       = aws_instance.db_server.private_ip
+}
+
+output "ssh_connection" {
+  description = "SSH command to connect to the web server"
+  value       = "ssh -i ~/.ssh/prestashop-key ubuntu@${aws_instance.web_server.public_ip}"
 }
