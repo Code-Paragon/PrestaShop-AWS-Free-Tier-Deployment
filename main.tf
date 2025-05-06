9db19d2b066e0afb8e88585e225db30bb872ba7d
Adedeji Mustapha <adedeji.mustapha.2018@gmail.com>
2025-05-06 18:36:07 +0100

    added infrastructure provisioning

---
+++ main.tf
@@ -0,0 +1,53 @@
+resource "aws_key_pair" "deployer" {
+  key_name   = var.key_name
+  public_key = file(var.public_key_path)
+}
+
+resource "aws_security_group" "prestashop_sg" {
+  name        = "prestashop-sg"
+  description = "Allow HTTP, HTTPS, and SSH"
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "prestashop" {
+  ami                         = "ami-041d287dbeca860a8"  # Ubuntu 20.04 LTS (Free-tier eligible)
+  instance_type               = var.instance_type
+  key_name                    = aws_key_pair.deployer.key_name
+  vpc_security_group_ids      = [aws_security_group.prestashop_sg.id]
+  subnet_id                   = var.subnet_id
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "Prestashop-Server"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,6 @@
+variable "aws_region" {}
+variable "instance_type" {}
+variable "key_name" {}
+variable "public_key_path" {}
+variable "vpc_id" {}
+variable "subnet_id" {}
